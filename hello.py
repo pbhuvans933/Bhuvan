03777544884d86400ab01a1205142cb2454f358f
Bhuvan <pbhuvans933@gmail.com>
2021-09-28 14:00:49 +0530

    find biggest number

---
+++ hello.py
@@ -0,0 +1,11 @@
+no1=int(input('enter a value'))
+no2=int(input('enter a value'))
+if(no1 < no2):
+    sum=no1+no2
+    print(sum)
+elif(no1>no2):
+    sub=no1-no2
+
+    print(sub)
+else:
+    print('Hello')
